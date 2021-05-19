156409adbe11a1c7742d2fe8c6193e5b5cb73f0a
iver777 <ivered777@gmail.com>
2021-05-18 20:38:13 -0400

    practica en clases

---
+++ src/test/java/ClientApi/RequestClient.java
@@ -0,0 +1,11 @@
+package ClientApi;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+public abstract class RequestClient {
+
+    Client client = ClientBuilder.newBuilder().build();
+
+    public abstract ResponseInformation send(RequestInformation request);
+}
